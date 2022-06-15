4a587d1bf813580763ef0e86b1b54c2ff83c0f37
Ariel Mendez <mendez123ariel@gmail.com>
2022-06-15 14:57:17 -0300

    cambios

---
+++ figuras.js
@@ -0,0 +1,16 @@
+// Este es el codido del cuadrado
+const ladoCuadrado =5
+console.log("Los lados del cuadrados miden: "+ ladoCuadrado+"cm");
+
+const perimetroCuadrado = ladoCuadrado *4;
+
+
+console.log("El perimetro de cuadrado es: "+ perimetroCuadrado+"cm");
+
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+
+console.log("El area del cuadrado es: "+ areaCuadrado + "cm^2");
+
+// Este es el codido del triangulo
